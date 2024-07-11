0cc913e8fdd4a880b06a775619fef3b74399cf59
HEDGEHOGyt <121992707+HEDGEHOGyt@users.noreply.github.com>
2024-07-12 02:02:29 +0300

    Create node.js

---
+++ node.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('message', (message) => {
+        console.log('Message received:', message);
+        io.emit('message', message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
